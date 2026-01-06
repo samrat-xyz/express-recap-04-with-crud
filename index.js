33b7b2f9b82ff1a80fb389a18e4acd6eea953a21
Asif-vai <mr3366533@gmail.com>
2026-01-06 11:19:01 +0600

    create api for delete single data

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
- 
-const port = process.env.PORT
-const app = express()
+
+const port = process.env.PORT;
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/',(req,res)=>{
-    res.send('express recap 04 server running....')
-})
+app.get("/", (req, res) => {
+  res.send("express recap 04 server running....");
+});
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const db = client.db('UsersDB');
-    const dbCollections = db.collection('users')
+    const db = client.db("UsersDB");
+    const dbCollections = db.collection("users");
 
-    app.post('/users',async(req,res)=>{
-        const newUser = req.body;
-        const result = await dbCollections.insertOne(newUser);
-        res.send(result)
-    })
-    app.get('/users',async(req,res)=>{
-        const cursor = await dbCollections.find().toArray();
-        res.send(cursor)
-    })
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await dbCollections.insertOne(newUser);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const cursor = await dbCollections.find().toArray();
+      res.send(cursor);
+    });
 
-    app.get('/users/:id',async(req,res)=>{
-        const id = req.params
-        const query = await dbCollections.findOne({_id : new ObjectId(id)})
-        res.send(query)
-    })
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = await dbCollections.findOne({ _id: new ObjectId(id) });
+      res.send(query);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await dbCollections.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,4 +62,4 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port)
+app.listen(port);
